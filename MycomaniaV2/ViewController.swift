46c7c83368cabb611be0b7743991a3ebd3f34d8a
maitreebain <maitreebain@gmail.com>
2020-04-07 23:26:59 -0400

    Initial Commit

---
+++ MycomaniaV2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MycomaniaV2
+//
+//  Created by Maitree Bain on 4/7/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MycomaniaV2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MycomaniaV2
+
+  target 'MycomaniaV2Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
