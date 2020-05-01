6fbff50391f571dacc112bd1b67414d01f4ec62f
maitreebain <maitreebain@gmail.com>
2020-05-01 00:02:57 -0400

    added launch screen

---
+++ MycomaniaV2/Designable.swift
@@ -0,0 +1,75 @@
+//
+//  Designable.swift
+//  MycomaniaV2
+//
+//  Created by Maitree Bain on 4/30/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class DesignableView: UIView {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableButton: UIButton {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableTextField: UITextField {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableImageView: UIImageView {
+  @IBInspectable var cornerRadius: CGFloat = 0
+  @IBInspectable var borderWidth: CGFloat = 0
+  @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layer.cornerRadius = cornerRadius
+    layer.borderWidth = borderWidth
+    layer.borderColor = borderColor
+  }
+}
+
+class DesignableLabel: UILabel {
+    @IBInspectable var cornerRadius: CGFloat = 0
+   @IBInspectable var borderWidth: CGFloat = 0
+   @IBInspectable var borderColor: CGColor = UIColor.black.cgColor
+   
+   override func layoutSubviews() {
+     super.layoutSubviews()
+     layer.cornerRadius = cornerRadius
+     layer.borderWidth = borderWidth
+     layer.borderColor = borderColor
+   }}
