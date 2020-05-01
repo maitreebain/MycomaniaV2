55c39ad40797e82745b5ae7394e759a90ef7ae56
maitreebain <maitreebain@gmail.com>
2020-04-30 20:53:56 -0400

    working on login screen and launch screen

---
+++ MycomaniaV2/View Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  MycomaniaV2
+//
+//  Created by Maitree Bain on 4/30/20.
+//  Copyright © 2020 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- MycomaniaV2/View Controllers/MapViewController.swift
+++ MycomaniaV2/View Controllers/MapViewController.swift
@@ -14,7 +14,6 @@ class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
